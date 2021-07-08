6fba2d50f8e3fb9384e048a8c707a582ebcf78a0
Suraj Verma <surajverma@rentalcars.com>
2021-07-08 19:43:18 +0100

    CRUD plans

--- src/controllers/api/plans.js
+++ src/controllers/api/plans.js
@@ -6,20 +6,47 @@ const getPlans = async (req, res) => {
 };
 
 const getPlan = async (req, res) => {
-  const plan = await Plan.findById(req.params.id).populate("locations");
+  const { id } = req.params;
+
+  const plan = await Plan.findById(id).populate("locations");
   res.json(plan);
 };
 
-const createPlan = (req, res) => {
-  res.json({});
+const createPlan = async (req, res) => {
+  const { title, locations } = req.body;
+
+  const plan = {
+    title,
+    locations,
+  };
+
+  const data = await Plan.create(plan).populate("locations");
+
+  res.json(data);
 };
 
-const updatePlan = (req, res) => {
-  res.json({});
+const updatePlan = async (req, res) => {
+  const { title, locations } = req.body;
+  const { id } = req.params;
+
+  const plan = {
+    title,
+    locations,
+  };
+
+  const data = await Plan.findOneAndUpdate({ _id: id }, plan, {
+    new: true,
+  }).populate("locations");
+
+  res.json(data);
 };
 
-const deletePlan = (req, res) => {
-  res.json({});
+const deletePlan = async (req, res) => {
+  const { id } = req.params;
+
+  const data = await Plan.findOneAndDelete({ _id: id }).populate("locations");
+
+  res.json(data);
 };
 
 module.exports = {
